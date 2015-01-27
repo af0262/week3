2df63edd6036894093629ae4625ab5478ab38098
Micky Simwenyi <msimwenyi@uchicago.edu>
2015-01-27 16:43:44 -0600

    Update divvy.py

--- divvy.py
+++ divvy.py
@@ -1,21 +1,35 @@
-# Divvy Bikes
-#
-# Here's an example of how to retrieve the list of Divvy bike stations:
-
 import json
-from urllib.request import urlopen
+from urllib.request import urlopen\
+
+
 
 webservice_url = "http://www.divvybikes.com/stations/json"
 data = urlopen(webservice_url).read().decode("utf8")
 result = json.loads(data)
-stations = result['stationBeanList']
-print(stations)
 
-# The Young building has the following latitude and longitude: 41.793414,-87.600915.
-# To measure surface distance, you can treat latitudes and longitudes like x and y coordinates, and calculate distance between locations with the usual Euclidean distance formula.
 
-# 1. Modify the code above to display the station name and number of available bikes for the station closest to Young.
+ide = result['stationBeanList'][276]['id']
+nam = result['stationBeanList'][276]['stationName']
+ava = result['stationBeanList'][276]['availableDocks']
+tot = result['stationBeanList'][276]['totalDocks']
+bik = result['stationBeanList'][276]['availableBikes']
+lat = result['stationBeanList'][276]['latitude']
+lon = result['stationBeanList'][276]['longitude']
+x2=41.793414
+y2=-87.600
 
-# You will likely want to consult the JSON stream from Divvy
+import math
+from math import sqrt
 
-# - http://www.divvybikes.com/stations/json
+dist = sqrt( (x2 - lat)**2 + (y2 - lon)**2 )
+
+
+print ("The nearest Station to The Young Building is", nam, "and has a station ID of",ide)
+print ("The longitude is", lon)
+print ("There are",tot, "Docks and",ava, "available")
+print ("There are",bik, "available bikes")
+print ("The latitude is:", lat)
+print ("The longitude is:", lon)
+print ("The distance between the young bulding the nearest divvy bike station is;", dist)
+
+
